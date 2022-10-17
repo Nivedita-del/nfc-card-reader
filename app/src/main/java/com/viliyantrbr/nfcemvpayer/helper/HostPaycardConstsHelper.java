9b1da93c2ede57b9b26bf66c65bc82e1fd763591
vishnu <x3x0n@localhost.localdomain>
2022-10-17 19:37:45 +0530

    init

---
+++ app/src/main/java/com/viliyantrbr/nfcemvpayer/helper/HostPaycardConstsHelper.java
@@ -0,0 +1,6 @@
+package com.nivedita.nfcemvpayer.helper;
+
+public class HostPaycardConstsHelper {
+    private static final String TAG = HostPaycardConstsHelper.class.getSimpleName();
+
+}
---
+++ app/src/main/java/com/viliyantrbr/nfcemvpayer/helper/ReadPaycardHelper.java
@@ -0,0 +1,5 @@
+package com.nivedita.nfcemvpayer.helper;
+
+public class ReadPaycardHelper {
+    private static final String TAG = ReadPaycardHelper.class.getSimpleName();
+}
